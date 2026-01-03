4a73cc41c9b0f3716d902ab92983a7241312a0a4
Sito <40501794+sito8943@users.noreply.github.com>
2026-01-03 19:30:23 +0100

    removing fields from check list

--- src/views/Checklists/components/ChecklistDialog.tsx
+++ src/views/Checklists/components/ChecklistDialog.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useMemo } from 'react'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Controller, useWatch } from 'react-hook-form'
+import { Controller } from 'react-hook-form'
 
 // @sito/dashboard
-import { TextInput, type Option, AutocompleteInput } from '@sito/dashboard'
+import { TextInput } from '@sito/dashboard'
 import { useAuth, FormDialog, ParagraphInput } from '@sito/dashboard-app'
 
 // types
@@ -17,7 +17,6 @@ import type {
 import { Tables } from 'lib/api'
 
 // hooks
-import { useCurrenciesCommon } from 'hooks'
 
 export function ChecklistForm(props: ChecklistFormPropsType) {
   const { control, isLoading, setValue, open } = props
@@ -27,15 +26,6 @@ export function ChecklistForm(props: ChecklistFormPropsType) {
   useEffect(() => {
     if (account && setValue) setValue('userId', account?.id ?? 0)
   }, [account, setValue, open])
-
-  const currencies = useCurrenciesCommon()
-
-  const currencyOptions = useMemo(
-    () => [...(currencies?.data ?? [])] as Option[],
-    [currencies.data]
-  )
-
-  const { id } = useWatch({ control })
 
   return (
     <>
@@ -66,49 +56,6 @@ export function ChecklistForm(props: ChecklistFormPropsType) {
             )}`}
             label={t('_entities:base.name.label')}
             placeholder={t('_entities:account.name.placeholder')}
-            {...rest}
-          />
-        )}
-      />
-      {!id && (
-        <Controller
-          control={control}
-          rules={{
-            required: `${t('_entities:account.balance.required')}`,
-          }}
-          name="balance"
-          disabled={isLoading}
-          render={({ field: { value, ...rest } }) => (
-            <TextInput
-              required
-              maxLength={20}
-              value={value ?? ''}
-              type="number"
-              autoComplete={`${Tables.Checklists}-${t(
-                '_entities:account.balance.initial'
-              )}`}
-              label={t('_entities:account.balance.initial')}
-              placeholder={t('_entities:account.balance.placeholder')}
-              {...rest}
-            />
-          )}
-        />
-      )}
-      <Controller
-        control={control}
-        name="currency"
-        disabled={isLoading}
-        render={({ field: { value, onChange, ...rest } }) => (
-          <AutocompleteInput
-            required
-            options={currencyOptions}
-            value={value}
-            autoComplete={`${Tables.Checklists}-${t(
-              '_entities:account.currency.label'
-            )}`}
-            onChange={v => onChange(v)}
-            label={t('_entities:account.currency.label')}
-            multiple={false}
             {...rest}
           />
         )}
